18c0b4bb1011c701a7429edcd57c862238a6ed51
Chanakarn <Chanakarn.kon@dome.tu.ac.th>
2024-11-26 20:09:17 +0700

    Add Data Validation On External JS.

--- script.js
+++ script.js
@@ -12,5 +12,28 @@ document.addEventListener("DOMContentLoaded", () =>
         const comments = document.getElementById("comments").value.trim();
         const rating = document.getElementById("rating").value;
         const options = document.getElementById("options").value;
+
+        //Data Validation
+        if (!name) 
+        {
+            alert("กรุณากรอกชื่อของคุณ");
+            return;
+        }
+        if (!comments) 
+        {
+            alert("กรุณากรอกความคิดเห็นของคุณ");
+            return;
+        }
+        if (!rating || rating < 1 || rating > 5) 
+        {
+            alert("กรุณาให้คะแนนระหว่าง 1 ถึง 5");
+            return;
+        }
+        if (!options) 
+        {
+            alert("กรุณาเลือกประเภทของความคิดเห็น");
+            return;
+        }
+        
     });
 });
